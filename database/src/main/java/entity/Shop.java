26a66e43b5bb65a45a6d7ba6a13f7c84c04edf22
denis.ivanov <denis.ivanov@axamit.com>
2018-06-08 12:30:13 +0300

    checkpoint 5 thymeleaf update

--- database/src/main/java/entity/Shop.java
+++ database/src/main/java/entity/Shop.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Set;
--- service/src/main/java/config/ServicePersistenceConfig.java
+++ service/src/main/java/config/ServicePersistenceConfig.java
@@ -1,27 +1,14 @@
 package config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
-
 @Configuration
-@PropertySource({"classpath:database.properties", "classpath:hibernate.properties"})
-@EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "service")
+@ComponentScan("service")
 @Import(PersistenceConfig.class)
 public class ServicePersistenceConfig {
 }
